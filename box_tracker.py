22a8d274bd953999e3e626192ccbe94e8285a82e
ahmdrz <ahmadrezazibaei@hotmail.com>
2017-05-21 20:47:23 +0430

    update

---
+++ box_tracker.py
@@ -0,0 +1,61 @@
+import math
+from random import randint
+import time
+
+
+class box_tracker():
+    def __init__(self, resolution=100, depth_resolution=120, ttl=100, movement=10):
+        self.boxes = []
+        self.resolution = resolution
+        self.depth_resolution = depth_resolution
+        self.ttl = ttl
+        self.movement = movement
+
+    def distance(self, a, b):
+        return math.floor(math.sqrt(math.pow(a[0] - b[0], 2) + math.pow(a[1] - b[1], 2)))
+
+    def get_center(self, rect):
+        try:
+            point = (rect[0] + rect[2] //
+                     2, rect[1] + rect[3] // 2)
+            return point
+        except:
+            return (0, 0)
+
+    def get_random_color(self):
+        return (randint(0, 255), randint(0, 255), randint(0, 255))
+
+    def init_frame(self):
+        for index, box in enumerate(self.boxes):
+            box['age'] = box['age'] - 1
+            if box['age'] < self.ttl // 2:
+                box['enable'] = False
+            else:
+                box['enable'] = True
+
+            if box['age'] == 0:
+                del self.boxes[index]
+                continue
+
+    def update_box(self, input_box, input_depth):
+        point = self.get_center(input_box)
+
+        detected = False
+        for index, box in enumerate(self.boxes):
+            box_point = self.get_center(box['rect'])
+            if (self.distance(box_point, point) < self.resolution) and (input_depth > box['depth'] - self.depth_resolution and input_depth < box['depth'] + self.depth_resolution):
+                if self.distance(box_point, point) > self.movement:
+                    box['rect'] = input_box
+                box['depth'] = input_depth
+                box['age'] = self.ttl
+                detected = True
+
+        if not detected:
+            box = {'rect': input_box, 'depth': input_depth,
+                   'color': self.get_random_color(), 'age': self.ttl, 'enable': True}
+            self.boxes.append(box)
+
+    def get_boxes(self):
+        for box in self.boxes:
+            if box['enable']:
+                yield box
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from freenect import sync_get_video as get_video
 from freenect import sync_get_depth as get_depth
+from box_tracker import box_tracker
 import cv2
 import sys
 
 cascadePath = "haarcascade_frontalface_alt2.xml"
 face_cascade = cv2.CascadeClassifier(cascadePath)
 alpha = 0.9
+tracker = box_tracker()
+
 
 def draw_face(d, output, color):
     scale_x = (d[0] + d[2] - d[0]) / 4
@@ -22,6 +25,7 @@ def draw_face(d, output, color):
     cv2.line(output, (d[0] + d[2], d[1] + d[3]),
              (d[0] + d[2], d[1] + d[3] - scale_y), color)
 
+
 while True:
     try:
         image, _ = get_video()
@@ -29,18 +33,19 @@ while True:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         blank_image = image.copy()
 
+        tracker.init_frame()
+
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for index,face in enumerate(faces):
-            draw_face(face, blank_image, (100, 200, 100))
-            face_center = (face[0] + face[2] // 2, face[1] + face[3] // 2)
-            cv2.circle(blank_image, face_center, 2, (0, 0, 200))
-            t_depth = depth[face_center[1], face_center[0]]
-            print index, t_depth
+        for face in faces:
+            face_center = tracker.get_center(face)
+            tracker.update_box(face, depth[face_center[1], face_center[0]])
+
+        for box in tracker.get_boxes():
+            draw_face(box['rect'], blank_image, box['color'])
 
         cv2.addWeighted(blank_image, alpha, image, 1 - alpha, 0, image)
         cv2.imshow("camera", image)
         cv2.waitKey(5)
     except KeyboardInterrupt:
         sys.exit(1)
-
